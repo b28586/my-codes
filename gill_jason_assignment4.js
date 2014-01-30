3ecde13e71fec78cbf31ff6972134baa0ba1611e
Jason Gill <thewiccan@fullsail.edu>
2014-01-30 12:51:59 -0600

    Week 4 Assignment

---
+++ gill_jason_assignment4.js
@@ -0,0 +1,139 @@
+// Find the smallest value in an array that is greater than a given number
+
+var smallValue = function(numArray, number) {
+        var largNums = [],
+                smallNum;
+        
+        for (i = 0; i < numArray.length; i++) {
+                if (numArray[i] > number) {
+                        largNums.push(numArray[i]);
+                }
+        }
+        
+        smallNum = largNums[0];
+        
+        for (j = 0; j < largNums.length; j++) {
+                if (smallNum > largNums[j]) {
+                        smallNum = largNums[j];
+                }
+        }
+        
+        return ("The smallest number that is greater than " + number + " is " + smallNum + ".");
+}
+
+
+// Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+
+
+var specificDec = function (number) {
+        var newNum;
+        
+        newNum = number.toFixed(2);
+        
+        return (number + " has been rounded to " + newNum);        
+}
+
+
+// Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
+
+var charReplace = function (string, oldChar, newChar) {
+        var myString = string,
+                old = new RegExp(oldChar, "g");
+        
+        myString = myString.replace(old, newChar);
+        
+        return myString;
+}
+
+
+// Is the string a URL? (Does it start with http: or https:?)
+
+var urlCheck = function (string) {
+        var url = string,
+                check = "",
+                checkArray = [],
+                end = url.indexOf(":");
+                
+        checkArray[0] = "http:";
+        checkArray[1] = "https:";
+                
+        check = url.substring(0, end + 1);
+        
+        if (check === checkArray[0]) {
+                return (url + " is a valid URL.");
+        }
+        else if (check === checkArray[1]) {
+                return (url + " is a valid URL.");
+        }
+        else {
+                return (url + " is not a valid URL.");
+        }
+}
+
+
+// Does a string follow an aaa@bbb.ccc pattern like an email address?
+
+var emailCheck = function (localEmail) {
+        var email = localEmail,
+                checkAt = 0,
+                checkDot = 0;
+                
+        checkAt = email.indexOf("@");
+        checkDot = email.indexOf(".");
+        
+        if (checkAt != -1 && checkDot != -1) {
+                if (checkAt < checkDot) {
+                        if (checkDot - checkAt != 1) {
+                                return (email + " is a valid email.");
+                        }
+                        else {
+                                return (email + " is not a valid email.");
+                        }
+                }
+                else {
+                        return (email + " is not a valid email.");
+                }
+        }
+        else {
+                return (email + " is not a valid email.");
+        }        
+}
+
+
+// Does a string follow a 123-456-7890 pattern like a phone number?
+
+var numberCheck = function (phoneNumber) {
+        var number = phoneNumber,
+                regExpPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/,
+                check;
+                
+        check = regExpPattern.test(number);
+        
+        if (check) {
+                return (number + " is a valid phone number.");
+        }
+        else {
+                return (number + " is not a valid phone number.");
+        }
+}
+
+var numArray = [5, 12, 15, 21, 38, 45, 43, 65, 123, 32, 12, 23]
+
+console.log(specificDec(2.1));
+console.log(specificDec(3.2123));
+console.log(specificDec(100));
+
+console.log(smallValue(numArray, 22));
+
+console.log(charReplace("A,B,C,D,E", ",", "/"));
+
+console.log(urlCheck("http://www.google.com/"));
+console.log(urlCheck("google.com"));
+
+console.log(emailCheck("thewiccan@fullsail.edu"));
+console.log(emailCheck("thewiccan@fullsailedu"));
+
+console.log(numberCheck("701-521-8767"));
+console.log(numberCheck("(912)-265-9786"));
+console.log(numberCheck("502-125-564"));
+
